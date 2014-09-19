ef5bb444dd980b75f5c8aee37b975db00a970674
Jack Stevens <jac.stevens@gmail.com>
2014-09-19 18:45:43 +0100

    Enhance logging and error handling

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
-var chalk = require('chalk'),
-    q = require('q');
+(function(exports) {
+  var chalk = require('chalk'),
+      q = require('q'),
+      trycatch = require('trycatch');
 
-exports.start = function(app) {
-  var port = app.get('port') || process.env.PORT || 3000;
-  var deferred = q.defer();
-  app.listen(port, function(){
-    var message = 'Server listening on port: ' + port;
-    console.log(chalk.green(" ✔ ") + message);
-    deferred.resolve();
-  });
-  return deferred.promise;
-};
+  function hideLogs () {
+    switch(process.env.HIDE_SE_LOG) {
+      case 'true':
+        return true;
+      case 'false':
+      case undefined:
+        return false;
+      default:
+        var message = 'Warning: Invalid value for HIDE_SE_LOG (must be "true" or "false")';
+        console.log(chalk.yellow(' > ') + message);
+        return false;
+    }
+  }
+
+  function logError (port) {
+    if(!hideLogs()) {
+      var message = 'Failed to start server on port: ' + port;
+      console.log(chalk.red(" ✘ ") + message);
+    }
+  }
+
+  function logSuccess (port) {
+    if(!hideLogs()) {
+      var message = 'Server listening on port: ' + port;
+      console.log(chalk.green(" ✔ ") + message);
+    }
+  }
+
+  exports.start = function(app) {
+    var port = app.get('port') || app.get('PORT') || process.env.PORT || 3000,
+        deferred = q.defer(),
+        httpServer;
+    trycatch(function(){
+      httpServer = app.listen(port, function(){
+        logSuccess(httpServer);
+        deferred.resolve(server);
+      });
+    }, function(err) {
+      logError(port);
+      deferred.reject(err);
+    });
+    return deferred.promise;
+  };
+
+})(module.exports);
